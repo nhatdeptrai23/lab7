d7c890c4c2e97e59da218fe7aab149af8affd423
nhatdeptrai23 <japnh24>
2023-06-07 15:47:48 +0700

    add

--- index.js
+++ index.js
@@ -14,4 +14,17 @@ function init() {
     user.addEventListener("mouseout", hide);
 }
 
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+});
+
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
+
 window.onload = init;
